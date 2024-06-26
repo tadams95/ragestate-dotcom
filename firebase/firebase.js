13625f54af364efe46a7dd75728db9a6561f761a
tadams95 <tyrelleadams28@gmail.com>
2024-06-25 18:48:27 -0700

    outsource some styles / working on firebase

---
+++ firebase/firebase.js
@@ -0,0 +1,31 @@
+// firebase.js
+
+import { initializeApp } from "firebase/app";
+import { initializeAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTHDOMAIN,
+  databaseURL: process.env.DATABASEURL,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APPID,
+  measurementId: process.env.MEASUREMENTID,
+};
+
+// Initialize Firebase app
+const app = initializeApp(firebaseConfig);
+
+
+// Initialize Firebase Firestore
+const db = getFirestore(app);
+
+// Initialize Firebase Storage
+const storage = getStorage(app);
+
+// Export the initialized app, auth, db, and storage
+export { app, db, storage };
--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,5 @@
+import HomeStyling1 from "../../components/styling/HomeStyling1";
+import HomeStyling2 from "../../components/styling/HomeStyling2";
 import Header from "./components/Header";
 
 export default function Home() {
@@ -6,18 +8,7 @@ export default function Home() {
       <div className="bg-black">
         <Header />
         <div className="relative isolate px-6 pt-14 lg:px-8">
-          <div
-            className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-            aria-hidden="true"
-          >
-            <div
-              className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#8A041A] to-[#FFFFFF] opacity-75 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-              style={{
-                clipPath:
-                  "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-              }}
-            />
-          </div>
+          <HomeStyling1 />
           <div className="mx-auto max-w-2xl py-20 pt-72">
             <div className="text-center">
               <h1 className="text-4xl font-bold tracking-tight text-gray-100 sm:text-5xl font-sans pt-10">
@@ -26,18 +17,7 @@ export default function Home() {
               <p className="mt-6 text-lg leading-8 text-gray-300"></p>
             </div>
           </div>
-          <div
-            className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-            aria-hidden="true"
-          >
-            <div
-              className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#8A041A] to-[#FFFFFF] opacity-75 sm:left-[calc(50%+20rem)] sm:w-[72.1875rem]"
-              style={{
-                clipPath:
-                  "polygon(70.1% 24.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-              }}
-            />
-          </div>
+          <HomeStyling2 />
         </div>
       </div>
       <p className="mt-60 text-center text-xs leading-5 text-gray-500">
