b5ef58e7cf2fdc688d96f9ad9598b4113c7a21c1
tadams95 <tyrelleadams28@gmail.com>
2024-07-04 12:50:00 -0700

    fix random detail styling warning

--- src/app/components/styling/RandomDetailStyling.js
+++ src/app/components/styling/RandomDetailStyling.js
@@ -1,12 +1,19 @@
 import { memo } from "react";
+import seedrandom from "seedrandom";
 
+// Function to generate a random clip path with a consistent seed
 const generateRandomClipPath = () => {
   const points = 16; // Number of points in the clip path polygon
+  const seed = "hello."; // Seed value for deterministic randomness
+
+  // Initialize seeded random number generator
+  const random = seedrandom(seed);
+
   let clipPath = "polygon(";
 
   for (let i = 0; i < points; i++) {
-    const x = Math.random() * 100; // Random x-coordinate percentage
-    const y = Math.random() * 100; // Random y-coordinate percentage
+    const x = random() * 100; // Random x-coordinate percentage
+    const y = random() * 100; // Random y-coordinate percentage
     clipPath += `${x.toFixed(1)}% ${y.toFixed(1)}%`; // Add point to clip path
     if (i < points - 1) clipPath += ", "; // Add comma for all points except the last one
   }
@@ -16,6 +23,9 @@ const generateRandomClipPath = () => {
 };
 
 const X = () => {
+  const clipPath1 = generateRandomClipPath();
+  const clipPath2 = generateRandomClipPath();
+
   return (
     <>
       <div className="relative isolate -z-10">
@@ -26,7 +36,7 @@ const X = () => {
           <div
             className="aspect-[701/1036] w-[50.0625rem] bg-gradient-to-tr from-[#FFF] to-[#000000] opacity-90"
             style={{
-              clipPath: generateRandomClipPath(),
+              clipPath: clipPath1,
             }}
           />
         </div>
@@ -39,7 +49,7 @@ const X = () => {
           <div
             className="aspect-[701/1036] w-[50.0625rem] bg-gradient-to-tr from-[#8A041A] to-[#000000] opacity-90"
             style={{
-              clipPath: generateRandomClipPath(),
+              clipPath: clipPath2,
             }}
           />
         </div>
