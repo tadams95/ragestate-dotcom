e3bf370f647698827af15a1cb0aa02a5d5a2d72c
tadams95 <tyrelleadams28@gmail.com>
2024-07-05 20:20:25 -0700

    work to implement payment element

---
+++ src/app/testPayment/page.js
@@ -0,0 +1,75 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "../../../components/CheckoutForm";
+import RandomDetailStyling from "../components/styling/RandomDetailStyling";
+
+export default function Payment() {
+  const [stripePromise, setStripePromise] = useState(null);
+  const [clientSecret, setClientSecret] = useState("");
+
+  useEffect(() => {
+    const publishableKey = process.env.NEXT_PUBLIC_stripe_testPk;
+
+    setStripePromise(loadStripe(publishableKey));
+  }, []);
+
+  useEffect(() => {
+    const fetchClientSecret = async () => {
+      try {
+        const response = await fetch(
+          "https://us-central1-ragestate-app.cloudfunctions.net/stripePayment/create-payment-intent",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({}),
+          }
+        );
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch payment intent");
+        }
+
+        const { client_secret } = await response.json();
+        setClientSecret(client_secret);
+        console.log("Client Secret: ", client_secret);
+      } catch (error) {
+        console.error("Error fetching payment intent:", error.message);
+      }
+    };
+
+    fetchClientSecret();
+  }, []);
+
+  const appearance = {
+    theme: "stripe",
+    variables: {
+        colorText: "#ffffff",
+        colorBackground: '#000000',
+    },
+  };
+  const options = {
+    clientSecret,
+    appearance,
+  };
+
+  return (
+    <>
+      <div className="mx-auto my-40 max-w-7xl px-4 sm:px-6 lg:px-8 isolate">
+        <RandomDetailStyling />
+        <div className="mx-auto max-w-3xl border border-1 p-8 rounded-md">
+          <h1>React Stripe and the Payment Element</h1>
+          {clientSecret && stripePromise && (
+            <Elements stripe={stripePromise} options={options}>
+              <CheckoutForm />
+            </Elements>
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
