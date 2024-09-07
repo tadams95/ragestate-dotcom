8cce2ff1976855bb39dfdf10ab146a8dbdd6a6d0
tadams95 <tyrelleadams28@gmail.com>
2024-09-07 11:05:24 -0700

    product fetching is more robust

--- src/app/shop/page.js
+++ src/app/shop/page.js
@@ -9,26 +9,41 @@ import Header from "../components/Header";
 export default function Shop() {
   const [productsWithHref, setProductsWithHref] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
+    let isMounted = true;
+
     const fetchData = async () => {
+      setLoading(true);
+      setError(null);
       try {
         const fetchedProducts = await fetchShopifyProducts();
-        const products = fetchedProducts.map((product) => ({
-          ...product,
-          href: `/product/${product.id}`,
-          imageSrc: product.images[0]?.src,
-          description: product.descriptionHtml,
-        }));
+        if (isMounted) {
+          const products = fetchedProducts.map((product) => ({
+            ...product,
+            href: `/product/${product.id}`,
+            imageSrc: product.images[0]?.src,
+            description: product.descriptionHtml,
+          }));
 
-        setProductsWithHref(products);
-        setLoading(false);
+          setProductsWithHref(products);
+          setLoading(false);
+        }
       } catch (error) {
-        console.error("Error fetching products:", error);
+        if (isMounted) {
+          console.error("Error fetching products:", error);
+          setError("Failed to fetch products. Please try again later.");
+          setLoading(false);
+        }
       }
     };
 
     fetchData();
+
+    return () => {
+      isMounted = false;
+    };
   }, []);
 
   return (
@@ -36,19 +51,23 @@ export default function Shop() {
       <Header />
 
       <div className="mx-auto max-w-7xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
-        <div
-          className={`mt-6 grid gap-y-10 lg:grid-cols-3 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-0 lg:gap-x-8 ${
-            loading
-              ? "opacity-0"
-              : "opacity-100 transition-opacity duration-1000"
-          }`}
-        >
-          {productsWithHref.map((product) => (
-            <div key={product.id}>
-              <ProductTile product={product} />
-            </div>
-          ))}
-        </div>
+        {loading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
+        {!loading && !error && (
+          <div
+            className={`mt-6 grid gap-y-10 lg:grid-cols-3 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-0 lg:gap-x-8 ${
+              loading
+                ? "opacity-0"
+                : "opacity-100 transition-opacity duration-1000"
+            }`}
+          >
+            {productsWithHref.map((product) => (
+              <div key={product.id}>
+                <ProductTile product={product} />
+              </div>
+            ))}
+          </div>
+        )}
       </div>
 
       <Footer />
