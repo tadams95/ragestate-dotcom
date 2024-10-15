a07201e52b453f446e2b865f64d9ed3807937544
tadams95 <tyrelleadams28@gmail.com>
2024-10-15 00:08:05 -0700

    LIVE IN YOUR WORLD, RAGE IN OURS

---
+++ src/app/components/animations/World.js
@@ -0,0 +1,50 @@
+"use client";
+
+import React, { useRef, useEffect } from "react";
+import { Canvas, useFrame, extend, useThree } from "@react-three/fiber";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import * as THREE from "three";
+
+extend({ OrbitControls });
+
+function RotatingSphere() {
+  const ref = useRef();
+  const texture = new THREE.TextureLoader().load("/assets/RAGESTATE.png");
+  const sphere = new THREE.SphereGeometry(1.5, 32, 32);
+
+  useFrame((state, delta) => {
+    ref.current.rotation.x -= delta / 10;
+    ref.current.rotation.y -= delta / 15;
+  });
+
+  return (
+    <group ref={ref} position={[0, -0.05, 0]}>
+      <mesh geometry={sphere}>
+        <meshBasicMaterial map={texture} />
+      </mesh>
+      <mesh geometry={sphere}>
+        <meshBasicMaterial color="#FFBDBD" wireframe={true} />
+      </mesh>
+    </group>
+  );
+}
+
+function Controls() {
+  const { camera, gl } = useThree();
+  const controls = useRef();
+  useFrame(() => controls.current.update());
+  return <orbitControls ref={controls} args={[camera, gl.domElement]} />;
+}
+
+export default function World() {
+  return (
+    <div className="absolute inset-0 z-0">
+      <Canvas camera={{ position: [0, 0, 4] }}>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <RotatingSphere />
+        <Controls />
+      </Canvas>
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -2,21 +2,18 @@ import HomeStyling1 from "./components/styling/HomeStyling1";
 import HomeStyling2 from "./components/styling/HomeStyling2";
 import Header from "./components/Header";
 import Home3DAnimation from "./components/animations/home-3d-animation";
+import World from "./components/animations/World";
 
 export default function Home() {
   return (
     <div className="bg-black min-h-screen relative overflow-hidden">
       <Home3DAnimation />
-      <div className="relative z-10">
+      <div className="relative z-1">
         <Header />
         <div className="relative isolate px-6 pt-14 lg:px-8">
           <HomeStyling1 />
-          <div className="flex items-center justify-center min-h-[82vh]">
-            <div className="text-center">
-              <h1 className="bg-clip-text font-bold text-transparent bg-gradient-to-r from-red-900 to-gray-100 text-4xl sm:text-5xl font-sans">
-                WELCOME TO RAGESTATE
-              </h1>
-            </div>
+          <div className="flex items-center justify-center min-h-[93vh] ">
+            <World />
           </div>
           <HomeStyling2 />
         </div>
