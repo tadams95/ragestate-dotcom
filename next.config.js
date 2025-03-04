b107c65d9b335bec70cbc9e1979a0370893808ec
tadams95 <tyrelleadams28@gmail.com>
2025-03-03 20:01:00 -0800

    feat: Add remote image patterns for Shopify, Firebase, and Unsplash in Next.js config

--- next.config.js
+++ next.config.js
@@ -2,13 +2,35 @@
 const nextConfig = {
   // Ensure SWC is used for compilation
   swcMinify: true,
-  
+
   // Disable Babel in favor of SWC
   experimental: {
     forceSwcTransforms: true,
   },
-  
-  // Your other Next.js configuration
-}
 
-module.exports = nextConfig
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.shopify.com",
+        port: "",
+        pathname: "/**", // Match all paths under the hostname
+      },
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+        port: "",
+        pathname: "/**", // Match all paths under the hostname
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**", // Match all paths under the hostname
+      },
+    ],
+  },
+};
+
+// Use CommonJS export instead of ES modules
+module.exports = nextConfig;
