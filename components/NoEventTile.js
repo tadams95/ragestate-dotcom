a2548e04e82a4ffc2f94c14f88baafa199e1e44e
tadams95 <tyrelleadams28@gmail.com>
2025-12-16 07:28:29 -0800

    feat: update video source in NoEventTile component and add new yacht party video asset

--- components/NoEventTile.js
+++ components/NoEventTile.js
@@ -4,7 +4,7 @@ export default function NoEventTile() {
       <div className="mx-auto my-8 w-full max-w-5xl">
         <div className="overflow-hidden rounded-lg border border-gray-900 bg-black">
           <video
-            src="/assets/SDSURAGE.mp4"
+            src="/assets/ragestate-yacht-party.mp4"
             aria-label="RAGESTATE"
             className="block h-auto w-full"
             controls
