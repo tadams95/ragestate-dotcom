1e5ade89e61982556453b2b9cee5b2ceb060d3e5
tadams95 <tyrelleadams28@gmail.com>
2026-03-05 15:12:29 -0800

    feat: refactor Facebook Pixel integration to improve script loading and remove unnecessary components

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,7 @@ import { Toaster } from 'react-hot-toast';
 import { FirebaseProvider } from '../../firebase/context/FirebaseContext';
 import { ChatUnreadProvider } from '../../lib/context/ChatUnreadProvider';
 import { ThemeProvider } from '../../lib/context/ThemeContext';
+import { FB_PIXEL_ID } from '../../lib/fpixel';
 import StoreProvider from './StoreProvider';
 import AuthCheck from './auth/AuthCheck';
 import FacebookPixel from './components/FacebookPixel';
@@ -45,6 +46,13 @@ export default function RootLayout({ children }) {
       <html lang="en" className="h-full" suppressHydrationWarning>
         <head>
           <script dangerouslySetInnerHTML={{ __html: themeScript }} />
+          {FB_PIXEL_ID && (
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','${FB_PIXEL_ID}');`,
+              }}
+            />
+          )}
         </head>
         <body className={`${inter.className} h-full`}>
           <ThemeProvider defaultTheme="dark">
